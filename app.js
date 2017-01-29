b723afb8b2562594a663dafce9ad5772c945d781
kimsuka13 <krame001@ucr.edu>
2017-01-28 17:33:35 -0800

    updating views

---
+++ app.js
@@ -0,0 +1,37 @@
+var myApp = angular.module('weatherApp',['ngRoute','ngResource']);
+myApp.config(function($routeProvider){
+	$routeProvider
+	.when('/',{
+		templateUrl: 'pages/home.html',
+		controller: 'homeController'
+		})
+	.when('/forecast',{
+		templateUrl: 'pages/forecast.html',
+		controller: 'forecastController'
+	})
+});
+
+myApp.service('nameService', function(){
+	this.name = "Santa Clara";
+});
+myApp.controller('homeController',['$scope','nameService',function($scope,nameService){
+	$scope.cityName="london";
+	$scope.$watch('cityName', function(){
+	nameService.name = $scope.cityName;
+});
+	
+}]);
+
+myApp.controller('forecastController',['$scope','$resource','nameService', function($scope,$resource, nameService){
+	$scope.cityName = nameService.name;
+	$scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily",{callback: "JSON_CALLBACK"},{	get: {	method: "JSONP"	}});
+	$scope.weatherResult = $scope.weatherAPI.get({q: $scope.cityName, cnt : 7, APPID: 'f2377b75c7705db7f2a250ff48d3378e'});
+	$scope.convertToF = function(deg){
+	return Math.round(1.8 * (deg - 273) + 32);
+	};
+	$scope.convertToDate = function(date){
+	var dt = new Date(date*1000);
+	return dt.toLocaleDateString("en-us",{month: 'short'}) +' '+dt.getDate();
+	};
+	// console.log($scope.weatherResult);
+}]);
